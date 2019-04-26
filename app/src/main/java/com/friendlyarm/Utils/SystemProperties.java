b7f18c95fec5ea0da865a456795f2466671d2782
xjc <huanglixjc>
2019-04-26 09:25:31 +0800

    1312331

---
+++ app/src/main/java/com/friendlyarm/Utils/SystemProperties.java
@@ -0,0 +1,30 @@
+package com.friendlyarm.Utils;
+
+import java.lang.reflect.Method;
+
+public class SystemProperties {
+
+    public static String get(String key) {
+        String value = "";
+        try {
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Method get = c.getMethod("get", String.class, String.class);
+            value = (String)(get.invoke(c, key, "unknown" ));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            return value;
+        }
+    }
+
+    public static void set(String key, String value) {
+        try {
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Method set = c.getMethod("set", String.class, String.class);
+            set.invoke(c, key, value );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
